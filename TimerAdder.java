52e280f77bd2ca6e47475e3197efec48355a04d5
broten15 <66185640+broten15@users.noreply.github.com>
2020-10-17 22:27:15 -0500

    finished without test cases

--- TimerAdder.java
+++ TimerAdder.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TimerAdder {
 
     private static final int MINS_IN_DAY = 1440;
@@ -6,7 +9,7 @@ public class TimerAdder {
     private static final int HOURS_IN_HALF_A_DAY = 12;
     public static void main(String[] args) {
         // tester for addMinutes
-        String result = addMinutes("4:55 PM", 100);
+        String result = addMinutes("12:00 PM", 0);
         System.out.println(result);
     }
 
@@ -25,60 +28,59 @@ public class TimerAdder {
     public static String addMinutes(String time, int minsToAdd) {
         if (time == null || !timeIsProperFormat(time)) {
             throw new IllegalArgumentException("time must be a string of must be in" +
-                                               "12-hour time format: [H]H:MM {AM|PM}");
+                                               " 12-hour time format: [H]H:MM {AM|PM}");
         }
         int minsSince12AM = convertToMinutes(time);
         minsSince12AM += minsToAdd;
-        // if the result of adding |minsToAdd| is over 1440 (24 hours) that means
-        // a new day is started so the minutes passed since 12 AM will be 0
-        minsSince12AM %= MINS_IN_DAY;
-        // if the mins to add is negative and it makes minsSince12AM negative,
-        // minsSince12AM is now the mins before 12 AM so convert it back into
-        // the mins since 12 AM
+        // the remainder is the time of the currennt day
+        minsSince12AM %= MINS_IN_DAY; 
+        // if minsSince12AM is negative, the minsSince12AM will be mins before 12 AM 
+        // so convert it back into the mins since 12 AM
         if (minsSince12AM < 0) {
             minsSince12AM = MINS_IN_DAY + minsSince12AM;
         }
-        String result = convertTo12HourTimeString(minsSince12AM);
-        return result;
+        return convertTo12HourTimeString(minsSince12AM);
     }
 
     // helper method for addMinutes:
-    // converts the hours and minutes into a 12-hour time format 
+    // converts the hours and minutes into a string in 12-hour time format
     private static String convertTo12HourTimeString(int minsSince12AM) {
         // convert MinsSince12AM to hours, mins, and period 
         int hours = minsSince12AM / MINS_IN_HOUR;
         int minutes = minsSince12AM % MINS_IN_HOUR;
-        // if it is 
         String period = "AM";
-        // 0 hours in 12 hour time is a time of 12
-        if (hours == 0) { // period is AM
+        if (hours == 0) { // 0 hours in 12 hour time is a time of 12 AM
             hours += HOURS_IN_HALF_A_DAY;
-        } else if (minsSince12AM > MINS_IN_12HOURS) { // period is PM
-            // if it is 12 hours past 12AM then it is the PM period of the day.
+        } else if (minsSince12AM >= MINS_IN_12HOURS) { // more than 
             period = "PM";
             // adjust hours that are over 12 to fit 12-hour time format
             if (hours > HOURS_IN_HALF_A_DAY) {
                 hours -= HOURS_IN_HALF_A_DAY;
             }
         }
-        return hours + ":" + minutes + " " + period;
+        // if minutes is a single digit add a 0 before the digit
+        String minutesStr = "" + minutes;
+        if (minutes < 10) {
+            minutesStr = 0 + minutesStr;
+        }
+        return hours + ":" + minutesStr + " " + period;
     }
 
     // helper method for addMinutes:
-    // converts the time string into minutes passed since 12 AM and returns 
-    // that amount of minutes
+    // Converts the time string into minutes passed since 12 AM. 
+    // Splits up time string into hours, minutes, and period of day
+    // and parse hours and minutes as ints.
     private static int convertToMinutes(String time) {
-        // split up time into hours, minutes, and period of day
-        // and parse hours and minutes as ints
-        String[] timePeriod = time.split(" ");
+        // timePeriod[0] = hour:mins, timePeriod[1] = AM or PM
+        String[] timePeriod = time.split(" "); 
+        // hourMin[0] = hours string, hourMin[1] = minutes string
         String[] hourMin = timePeriod[0].split(":");
         int minsSince12AM = Integer.parseInt(hourMin[1]); 
-        // add the appropriate amount of mins to minSince12AM based on the period
-        // of day and the hour from the time string 
+        // add  appropriate amount of mins to minSince12AM 
         if (!(timePeriod[1].equals("AM") && hourMin[0].equals("12"))) {
-            // add the minutes from the hours from time string unless the hours is 12 AM 
+            // add the minutes from the hours in time string unless the hours is 12 AM 
             // (12AM is 0 mins since 12AM)
-            minsSince12AM += Integer.parseInt(hourMin[0]) * 60; // convert hours to mins
+            minsSince12AM += Integer.parseInt(hourMin[0]) * MINS_IN_HOUR; // convert hours to mins
             if (timePeriod[1].equals("PM") && !hourMin[0].equals("12")) {
                 // if it is PM and but not 12PM add an additional 12 hours (720 mins)
                 minsSince12AM += MINS_IN_12HOURS;
@@ -89,35 +91,11 @@ public class TimerAdder {
 
     // checks to make sure the the passed time string is of the proper format
     private static boolean timeIsProperFormat(String time) {
-        // check the length of the string
-        if (time.length() < 7 || time.length() > 8) {
-            return false;
-        }
-        // make sure it contains a colon and a space
-        if (!(time.contains(":") && time.contains(" "))) {
-            return false;
-        }
-        // check to make sure characters between colon are digits
-        String[] timePeriod = time.split(" ");
-        String[] hourMin = timePeriod[0].split(":");
-        for (int i = 0; i < hourMin.length; i++) {
-            for (int j = 0; j < hourMin[i].length(); j++) {
-                if (hourMin[i].charAt(j) < '0' || 
-                    hourMin[i].charAt(j) > '9') {
-                    return false;
-                }
-            }
-        }
-        // in time string make sure hour is less than or equals to 12
-        // and make sure minutes is less than or equals to 59
-        if (Integer.parseInt(hourMin[0]) > 12 || Integer.parseInt(hourMin[1]) > 59) {
-            return false;
-        }
-        // check to make sure period after time is AM or PM
-        if (!(timePeriod[1].equals("AM") || timePeriod[1].equals("PM"))) {
-            return false;
-        }
-        // all checks are passed, time string is of proper form
-        return true;
+        // Regex to check valid time in 12-hour format. 
+        String regexPattern = "(1[012]|[1-9]):[0-5][0-9](\\s)(?i)(AM|PM)"; 
+        Pattern compiledPattern = Pattern.compile(regexPattern); 
+        Matcher m = compiledPattern.matcher(time); 
+        // Return if the time matched the ReGex 
+        return m.matches(); 
     }
 }
